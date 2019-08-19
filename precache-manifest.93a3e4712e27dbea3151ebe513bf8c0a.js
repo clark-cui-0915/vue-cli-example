86b0f993a631d78311ea2515d8d1249e8767f49a
Cui, Yong (Clark) <clark.cui@polycom.com>
2019-08-19 14:45:04 +0800

    deploy

---
+++ precache-manifest.93a3e4712e27dbea3151ebe513bf8c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcdf672394112c9e72f",
+    "url": "/vue-cli-example/js/about.f8270709.js"
+  },
+  {
+    "revision": "c18e65f4c21345ba0ab1",
+    "url": "/vue-cli-example/css/app.d447cd7c.css"
+  },
+  {
+    "revision": "c18e65f4c21345ba0ab1",
+    "url": "/vue-cli-example/js/app.b795b3c1.js"
+  },
+  {
+    "revision": "7009cf92910b0dd61b49",
+    "url": "/vue-cli-example/js/chunk-vendors.5de009e4.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-cli-example/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f773f668b9a66cdd8893cbf701b9d8cb",
+    "url": "/vue-cli-example/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-cli-example/robots.txt"
+  }
+];
